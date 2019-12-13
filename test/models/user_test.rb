323b402eb6a0671840a1f6719b8f19315bf71186
Inou Ridder <inouridder@gmail.com>
2019-12-13 10:57:56 +0100

    ALL THE LANGUAGES! : )

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#full_name, it should return the full name composed of first and last names' do
+    user = User.new(first_name: 'Claire', last_name: 'Gautier')
+    result = user.full_name
+    assert_equal 'Claire Gautier', result
+  end
+
+  test '#full_name, it should return the full name with proper capitalization' do
+    user = User.new(first_name: 'claIre', last_name: 'gautIER')
+    result = user.full_name
+    assert_equal 'Claire Gautier', result
+  end
+
 end
